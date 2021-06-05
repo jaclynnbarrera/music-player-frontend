b00c5750926f28d562ed5d9e43fb04f0706b8e02
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-06-05 15:40:01 -0400

    add CommentInput class component where a form will be rendered to be able to comment on a song

---
+++ src/components/CommentInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class CommentInput extends React.Component {
+
+    render(){
+        return (
+            <div>
+                Comment Input
+            </div>
+        )
+    }
+}
+
+export default CommentInput
--- src/containers/CommentsContainer.js
+++ src/containers/CommentsContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CommentInput from '../components/CommentInput'
 
 class CommentsContainer extends React.Component {
 
@@ -6,6 +7,7 @@ class CommentsContainer extends React.Component {
         return (
             <div>
                 Commennnntssss container
+                <CommentInput />
             </div>
         )
     }
