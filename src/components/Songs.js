d81c74f0e23481085a8d05108e437a043e90adec
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-07-08 18:21:37 -0400

    minor styling updates

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -15,9 +15,9 @@ class Songs extends React.Component {
               <p>
                 {song.title} - {song.artist}
               </p>
-              {song.categories.map((category) => (
+              {/* {song.categories.map((category) => (
                 <p className="category">{category}</p>
-              ))}
+              ))} */}
               <Button />
             </div>
           ))}
