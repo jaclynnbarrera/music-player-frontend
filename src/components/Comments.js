2f25c0b147b6c97cc7377d0ae1f23d35bff8d245
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-07-15 22:29:57 -0400

    comment box styling change

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,9 +3,12 @@ import React from "react";
 const Comments = (props) => {
   console.log("comments");
   return (
-    <div className="comments-box">
-      {props.comments &&
-        props.comments.map((comment, i) => <p key={i}>{comment.content}</p>)}
+    <div>
+      <p>COMMENTS</p>
+      <div className="comments-box">
+        {props.comments &&
+          props.comments.map((comment, i) => <p key={i}>{comment.content}</p>)}
+      </div>
     </div>
   );
 };
