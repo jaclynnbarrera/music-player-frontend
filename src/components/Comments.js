82a09f49d374a86cefe61aad34785a74fd291007
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-07-15 23:17:19 -0400

    update comment components to render date in readable format and totala count of comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,12 +2,28 @@ import React from "react";
 
 const Comments = (props) => {
   console.log("comments");
+
+  // const date = props.comments[1]["created_at"].split("-")[2].split("T")[0];
+  // const month = props.comments[1]["created_at"].split("-")[1];
+  // const year = props.comments[1]["created_at"].split("-")[1];
   return (
     <div>
-      <p>COMMENTS</p>
+      <p>
+        <b>{props.comments.length} COMMENTS</b>
+      </p>
       <div className="comments-box">
         {props.comments &&
-          props.comments.map((comment, i) => <p key={i}>{comment.content}</p>)}
+          props.comments.map((comment, i) => (
+            <p key={i}>
+              <em>
+                {comment.created_at.split("-")[1]}/
+                {comment.created_at.split("-")[2].split("T")[0]}/
+                {comment.created_at.split("-")[0]}
+              </em>
+              <br></br>
+              {comment.content}
+            </p>
+          ))}
       </div>
     </div>
   );
