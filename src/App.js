8289e1caddbda5ab6b3b9cb260d9cf1ac9d13cd3
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-09-23 14:42:19 -0400

    adds illustration to title

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Scroll";
 import Home from "./components/Home";
+import sketch from "./images/sketch.png";
 
 class App extends React.Component {
   render() {
@@ -11,7 +12,9 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <h4 className="headline">RADIO 135</h4>
+        <h4 className="headline">
+          RADIO 135 <img src={sketch} alt="sketch" />
+        </h4>
         <NavBar />
         <Footer />
         <Home />
