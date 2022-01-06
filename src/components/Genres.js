92c989a345a499a7f5a4ba928a449052c8b4f9f3
jaclynnbarrera <jaclynnbarrera@gmail.com>
2022-01-06 03:33:58 -0500

    csssssss

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -9,36 +9,15 @@ const Genres = () => {
         <p>Browse music by your favorites genres or discover new favorites.</p>
       </div>
       <div className="right-column">
-        <div class="genres-grid">
-          <div class="row">
-            <div class="column">
-              <div class="orange-column">Some Text in Column One</div>
-            </div>
-
-            <div class="column">
-              <div class="blue-column">Some Text in Column Two</div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="column">
-              <div class="orange-column">Some Text in Column One</div>
-            </div>
-
-            <div class="column">
-              <div class="blue-column">Some Text in Column Two</div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="column">
-              <div class="orange-column">Some Text in Column One</div>
-            </div>
-
-            <div class="column">
-              <div class="blue-column">Some Text in Column Two</div>
-            </div>
-          </div>
+        <div className="genres-grid">
+          <div className="genre">ONE</div>
+          <div className="genre">ONE</div>
+          <div className="genre">ONE</div>
+          <div className="genre">ONE</div>
+          <div className="genre">ONE</div>
+          <div className="genre">ONE</div>
+          <div className="genre">ONE</div>
+          <div className="genre">ONE</div>
         </div>
       </div>
     </section>
