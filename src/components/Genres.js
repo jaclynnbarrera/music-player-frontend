7c3390d876dcb4cdd59517c24322a4ef2380c549
jaclynnbarrera <jaclynnbarrera@gmail.com>
2022-01-06 02:01:06 -0500

    add responsive flexbox for genres categories

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -4,17 +4,42 @@ import React from "react";
 const Genres = () => {
   return (
     <section class="genres-container">
-      <div>
-        <h1>Pick from your favorite genres or disover new ones.</h1>
+      <div className="left-column">
+        <h1>Explore Genres</h1>
+        <p>Browse music by your favorites genres or discover new favorites.</p>
       </div>
+      <div className="right-column">
+        <div class="genres-grid">
+          <div class="row">
+            <div class="column">
+              <div class="orange-column">Some Text in Column One</div>
+            </div>
 
-      <div class="genres-grid">
-        <div>genre</div>
-        <div>genre</div>
-        <div>genre</div>
-        <div>genre</div>
-        <div>genre</div>
-        <div>genre</div>
+            <div class="column">
+              <div class="blue-column">Some Text in Column Two</div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="column">
+              <div class="orange-column">Some Text in Column One</div>
+            </div>
+
+            <div class="column">
+              <div class="blue-column">Some Text in Column Two</div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="column">
+              <div class="orange-column">Some Text in Column One</div>
+            </div>
+
+            <div class="column">
+              <div class="blue-column">Some Text in Column Two</div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
